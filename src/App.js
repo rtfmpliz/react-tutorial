412edd556c2e32738337e4b7b6d12a3fd5a83ff0
wahyu <wahyu@istw.co.id>
2020-09-13 11:04:28 +0700

    pakai props untuk tampilkan one way data

--- src/App.js
+++ src/App.js
@@ -2,10 +2,30 @@ import React, { Component } from 'react'
 import Table from './Table'
 
 class App extends React.Component {
+
+    
     render() {
+        const characters = [
+            {
+                name: 'Charlie',
+                job: 'Janitor',
+            },
+            {
+                name: 'MAc',
+                job: 'Bouncer',
+            },
+            {
+                name: 'Dee',
+                job: 'Aspiring actress',
+            },
+            {
+                name: 'Dennis',
+                job: 'Bartender',
+            }
+        ]
         return (
             <div className="container">
-                <Table/>
+                <Table characterData={characters}/>
             </div>
         )
     }
