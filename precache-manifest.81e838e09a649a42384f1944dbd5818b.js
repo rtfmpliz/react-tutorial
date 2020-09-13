8e8c890eb987151265efec0fac5209bad6779d5e
wahyu <wahyu@istw.co.id>
2020-09-13 19:35:08 +0700

    Updates

---
+++ precache-manifest.81e838e09a649a42384f1944dbd5818b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c26702546ecb16c010a224309ecb9e20",
+    "url": "/react-tutorial/index.html"
+  },
+  {
+    "revision": "e9f16464fd9d3568c261",
+    "url": "/react-tutorial/static/css/main.f3900a30.chunk.css"
+  },
+  {
+    "revision": "9d2ae3c2628a4c5fd0d4",
+    "url": "/react-tutorial/static/js/2.67691ccd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-tutorial/static/js/2.67691ccd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9f16464fd9d3568c261",
+    "url": "/react-tutorial/static/js/main.83675a71.chunk.js"
+  },
+  {
+    "revision": "d2980c81d16204d1adff",
+    "url": "/react-tutorial/static/js/runtime-main.fba2e450.js"
+  }
+]);
